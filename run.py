3600003ddc07ef1410440e9b98e16c1ab5d12139
RealLordzombieboy <lordzombieboy@gmail.com>
2024-11-22 22:20:32 -0500

    Added More Detailed Comments. Fixed many Bugs
    Removed completed and obsolete TODOs.
    
    Removed fuel from rocket, as there is a universal fuel for both the binary values of fuel (arr[]) and if the rocket has fuel or not (fuel).
    
    Added doc strings and sufficient comments.
    
    Changed yellow print of Checkpoint to vibrant yellow, and regular yellow for deactivated Checkpoints.
    
    Fixed rocket avoidance system.
    
    Added SpaceObject in the way of the rocket in stage one to show avoidance system.
    
    Encountered issue where b1-b8 in arr are not being set to True or False propositions, but rather set arr[i] to the value True or False. Not solved.

--- run.py
+++ run.py
@@ -19,7 +19,7 @@ class BasicPropositions:
     def _prop_name(self):
         return f"A.{self.data}"
 
-#TODO Find out how to set b1-b8 to True or False
+#TODO Find out how to set b1-b8 to True or False. Seems like what we set is not considered and can and is overwritten
 b1 = BasicPropositions("b1")
 b2 = BasicPropositions("b2")
 b3 = BasicPropositions("b3")
@@ -41,30 +41,27 @@ stage = 1
 #  what the expectations are.
 def example_theory():
     # Base constraints before calculations:
-    E.add_constraint((~b1 & ~b2 & ~b3 & ~b4 & ~b5 & ~b6 & ~b7 & ~b8) >> ~fuel) # If fuel = 0, there is no fuel
-
+    E.add_constraint((~b1 & ~b2 & ~b3 & ~b4 & ~b5 & ~b6 & ~b7 & ~b8) >> ~fuel) # If fuel = 0, there is no fuel.
+    # E.add_constraint((b1 | b2 | b3 | b4 | b5 | b6 | b7 | b8) >> fuel) # If fuel > 0, there is fuel. Currently creates unsolvable solution error if added.
+    
     # Ask user for initial conditions
     enter_fuel()
 
     # Calculations:
 
-    # Repeat for each grid, modified to fit each stage's needs:
-    radius = 3
-    grid1 = create_grid(radius, 1)
-    grid2 = create_grid(radius, 2)
-    grid3 = create_grid(radius, 3)
+    grid1 = create_grid(RADIUS, 1)
+    grid2 = create_grid(RADIUS, 2)
+    grid3 = create_grid(RADIUS, 3)
+
+    add_to_grid(grid1, 3, RADIUS//2, SpaceObject(P=True)) # Adds asteroid to demonstrate rocket avoidance proceedure.
 
     universe = [grid1, grid2, grid3]
-    journey = rocket_dynamics(universe, radius)
+    journey = rocket_dynamics(universe, RADIUS)
 
     add_to_grid(grid2, 0, 0, Checkpoint(P=False, active=True))
     debug_print(grid2)
 
-    # TODO: Add rocket to grid
-    # TODO: Add checkpoint(s) to grid
-
-    # End repeat
-
+    # Unused concept to calculate rocket movement and run fuel_calc() in propositional logic (all 3 while loops):
     while (stage == 1):
         break
         # TODO: Add constraints to calculate the path, if nothing in the way should go straight right
@@ -80,18 +77,11 @@ def example_theory():
         # TODO: Add constraints to calculate the path, if nothing in the way should go straight right
         # TODO: Add constraint that stops path from being calculated if next cell to the right is a planet
 
-    #TODO Remove below code before submitting
-
-    # You can also add more customized "fancy" constraints. Use case: you don't want to enforce "exactly one"
-    # for every instance of BasicPropositions, but you want to enforce it for a, b, and c.:
-    # constraint.add_exactly_one(E, a, b, c)
-
     return E
 
 @proposition(E)
 class Rocket: 
-    def __init__(self, fuel, x, y):
-        self.fuel = fuel
+    def __init__(self, x, y):
         self.x = x
         self.y = y
 
@@ -119,6 +109,9 @@ class Checkpoint:
     def _prop_name(self):
         return f"A.{self.data}"
 
+"""
+Currently unused and unfinished function.
+"""
 def fly(grid):
     assert fuel, "Rocket is out of fuel."
     #TODO something needs to put the rocket in the grid before it can fly
@@ -127,7 +120,9 @@ def fly(grid):
 @proposition(E)
 class PlanetCell:
     def __init__(self, P):
-        self.Pf = P #boolean for placement
+        # Pf indicates if the rocket cannot pass into the object (i.e. Rocket cannot crash into a planet, Pf=True implies object. 
+        # Pf=False also can imply nothing is there, with the exception of Checkpoints).
+        self.Pf = P
     
     def _prop_name(self):
         return f"A.{self.data}"
@@ -145,7 +140,10 @@ def get_position(object):
     return (object.x, object.y)
 
 """
-Creates grid given the radius of the planet held within.
+Creates grid given the radius of the planet held within and the stage of the rocket's journey.
+@param stage takes in a value of either 1, 2, or 3, specifying the stage and thus the structure of the grid (stage 2 has the planet in the center of the grid,
+stage 1 on the left, stage 2 on the right)
+@param radius, the radius of the planet in the grid. Radius of 2 generates a 2x2 cell planet, though stage 1 and 3 only show one collumn of the planet in the grid.
 @return the grid as a 2D array with a planet with the specified radius at its center.
 """
 def create_grid(radius, stage):
@@ -163,41 +161,41 @@ def create_grid(radius, stage):
     for (y,x) in planet_coord:
         
         if 0 <= x < rows and 0 <= y < rows:
-            grid[y][x].Pf = True #set to true given coordinates
-            #TODO: Have to make sure the coordinate generating function makes sense: 
-            # For example, we cannot have a planet of radius 2 on the first row. 
+            grid[y][x].Pf = True # Grid's order is in [y][x] to improve spacial locality as movement in the x axis is more common.
     print("Checkpoint positions:")
     if (stage == 2 or stage == 3):
         print("x =", radius, end="\ny = ")
-        for i in range(rows):
+        for i in range(rows): # Places checkpoints on the far right edge of the planet in stage 2, and the cell to the left of the planet in stage 3.
             if grid[i][radius].Pf == False:
-                grid[i][radius] = Checkpoint() # TODO: Add different colour to print for checkpoint
+                grid[i][radius] = Checkpoint()
                 
-                print(i, end="; ")
+                print(i, end="; ") # DEBUG (Shows y position of current Checkpoint)
     else:
         print("x =", radius + 1, end="\ny = ")
-        for i in range(rows):
+        for i in range(rows): # Stage 1 needs to end at the end of the grid, so Checkpoints are placed there.
             if grid[i][radius + 1].Pf == False:
-                grid[i][radius + 1] = Checkpoint() # TODO: Add different colour to print for checkpoint
+                grid[i][radius + 1] = Checkpoint()
                 
-                print(i, end="; ") # DEBUG
+                print(i, end="; ") # DEBUG (Shows y position of current Checkpoint)
 
     print()
     
     debug_print(grid) # DEBUG
     return grid
 
-# DEBUG Print
+"""
+Prints given grid such that cells containing a planet print True in green, cells with a Checkpoint print False in yellow, and everything else prints False in red.
+"""
 def debug_print(grid):
     for row in grid:
         for cell in row:
             if (cell.Pf):
                 print("\033[32m", end="")
-            # essentially, want the checkpoints to be non-active (yellow) until the rocket gets there
+            # Essentially, want the checkpoints to be active (yellow) until the rocket gets there, then change to a duller colour
             elif (type(cell) == Checkpoint): # Cannot check Checkpoint class as it is a @proposition
                 print("I'm a checkpoint!") # DEBUG
                 if (cell.active == True):
-                    print("\033[33m", end="")
+                    print("\033[93m", end="")
                 else:
                     print("\033[33m", end="")
             else:
@@ -222,14 +220,13 @@ Sets the planet positions for all 3 stages.
 def planet_position(radius, stage):
     planet = []
     h=0
-    midpoint = (radius + 2) // 2
 
     if (stage == 1):
         for y in range(1, radius+1):
             planet.append((y, 0))
         return planet
     
-    #TODO Note that we can have planets of different radius in each stage. Same for now for testing purpose. 
+    #TODO Note that we can have planets of different radius in each stage. Same for now for testing purpose, will change later. 
 
     if (stage == 2):
         for x in range(1, radius + 1):
@@ -253,37 +250,36 @@ Rocket dynamics function maps the moving of the rocket in all 3 stages.
 def rocket_dynamics(universe, radius, stage=1):
     journey = []
     if (stage == 1):
-        grid = universe[0] #grid 1 for stage 1
+        grid = universe[0] # Grid 1 for stage 1
 
-        launch = (int)(radius//2) # Rocket starts in the middle of the planet
-        rocket = Rocket(fuel=arr, x=1, y=launch)
+        launch = radius//2 # Rocket starts in the middle of the planet
+        rocket = Rocket(x=1, y=launch)
 
-        add_to_grid(grid, rocket.x, rocket.y, SpaceObject(P=True)) # add the rocket to the grid
-        journey.append(get_position(rocket)) # add initial position to map
+        add_to_grid(grid, rocket.x, rocket.y, SpaceObject(P=True)) # Uses SpaceObject as a placeholder for the Rocket in the grid to visualize.
+        journey.append(get_position(rocket)) # Add initial position to map
 
         debug_print(grid)
 
-        while rocket.x < radius+1: # before the rocket reaches the first checkpoints (last column of grid 1)
-            x, y = get_position(rocket)
+        while rocket.x < radius+1: # Loop until end of grid, where the stage will switch
+            x, y = get_position(rocket) # Position before move
 
-            # in grid 1, rocket will move rightward always, unless blocked by a space object, in which case it moves up/down
+            # In grid 1, rocket will move rightward always, unless blocked by a space object, in which case it moves up/down
             if grid[y][x+1].Pf:
                 if x-1 >= 0 and not grid[y-1][x].Pf:
-                    move(rocket, 'x', 1)
-                if x+1 <= radius + 1 and not grid[y+1][x].Pf:
-                    move(rocket, 'x', -1)
-                    
+                    move(rocket, 'y', 1)
+                elif x+1 <= radius + 1 and not grid[y+1][x].Pf:
+                    move(rocket, 'y', -1)
             else:
                 move(rocket, 'x', 1)
-            x, y = get_position(rocket)
 
-            #TODO Figure out how to switch to stage 2/grid 2 once reaching the checkpoint (last column of grid 1)
+            #TODO Figure out how to switch to stage 2/grid 2 once reaching the Checkpoint (last column of grid 1)
             
             journey.append(get_position(rocket))
             add_to_grid(grid, rocket.x, rocket.y, SpaceObject(P=True))
+            grid[y][x].Pf=False # Sets previous Rocket position to empty space
             debug_print(grid)
         
-        print("Journey Path: ") #print journey coordinates of rocket
+        print("Journey Path: ") # Print the journey coordinates (path) of the rocket
         for step in journey:
             print(f"({step[0]}, {step[1]})", end=" -> ")
         print()
@@ -296,9 +292,11 @@ def enter_fuel() -> None:
     assert len(fuel_str) < 9, "Your number is more than 8 digits"
     i = 0
     for num in fuel_str:
-        if (num == 1):
-            arr[i] = True
-        elif (num == 0):
+        if (num == '1'):
+            arr[i] = True # This just changed arr[i] to True, not set the proposition to True. How do we set the proposition's status?
+                          # Idea: Create two classes, one with the constraint that it is always True, one that it is always False, then set b1-b8 to be one of those
+                          # propositions and change/set a new b{x} to the other when that b{x} is changed.
+        elif (num == '0'):
             arr[i] = False
         else:
             print("The character at index", i, "cannot be accepted.")
@@ -307,6 +305,7 @@ def enter_fuel() -> None:
     if (i<7):
         for j in range(i, 8): # If the binary number entered is less than 8 digits, set the remaining binary values to false.
             arr[j] = False
+    print(arr)
 
 """ Subtract 1 to arr (value of fuel). If arr is filled with false (Fuel = 0) sets fuel to False and ends process, returning fuel.
 """
@@ -317,8 +316,7 @@ def fuel_calc():
             break
         i = True
     if (not b1 and not b2 and not b3 and not b4 and not b5 and not b6 and not b7 and not b8):
-        fuel = False
-        #TODO end process here, return result fuel
+        fuel = False # TODO: Does not set universal proposition "fuel" to False? Why?
 
 # Included code as reference:
 
@@ -327,20 +325,20 @@ def fuel_calc():
 # that are instances of this class must be true by using a @constraint decorator.
 # other options include: at most one, exactly one, at most k, and implies all.
 # For a complete module reference, see https://bauhaus.readthedocs.io/en/latest/bauhaus.html
-@constraint.at_least_one(E)
-@proposition(E)
-class FancyPropositions:
+# @constraint.at_least_one(E)
+# @proposition(E)
+# class FancyPropositions:
 
-    def __init__(self, data):
-        self.data = data
+#     def __init__(self, data):
+#         self.data = data
 
-    def _prop_name(self):
-        return f"A.{self.data}"
+#     def _prop_name(self):
+#         return f"A.{self.data}"
 
 # At least one of these will be true
-x = FancyPropositions("x")
-y = FancyPropositions("y")
-z = FancyPropositions("z")
+# x = FancyPropositions("x")
+# y = FancyPropositions("y")
+# z = FancyPropositions("z")
 
 if __name__ == "__main__":
 
@@ -350,7 +348,7 @@ if __name__ == "__main__":
     # After compilation (and only after), you can check some of the properties
     # of your model:
     print("\nSatisfiable: %s" % T.satisfiable())
-    # print("# Solutions: %d" % count_solutions(T)) # This doesn't work, produces error
+    # print("# Solutions: %d" % count_solutions(T)) # This doesn't work, produces error!
 
     S = T.solve()
 
